9f47115985ddbc7ceb52d965186c46a9df5e3647
Rahul Borana <boranarahul28@gmail.com>
2026-02-15 15:03:02 +0530

    fixed port for render

--- server.js
+++ server.js
@@ -175,4 +175,6 @@ reply = "🪷 Krishna says:\n\n" + reply;
  }
 });
 
-app.listen(3000,()=>console.log("Bot running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Bot running on port " + PORT));
+
